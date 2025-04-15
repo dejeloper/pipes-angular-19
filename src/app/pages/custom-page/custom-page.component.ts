e318175ed217836502235f89f94d9b3f441eaae3
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-04-14 23:07:13 -0500

    first commit

---
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'custom-page',
+  imports: [],
+  templateUrl: './custom-page.component.html',
+})
+export default class CustomPageComponent { }
---
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'numbers-page',
+  imports: [],
+  templateUrl: './numbers-page.component.html',
+})
+export default class NumbersPageComponent { }
