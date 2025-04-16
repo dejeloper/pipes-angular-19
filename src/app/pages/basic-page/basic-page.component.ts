0fd47bbe360a6b60c03868a6759a1856135c1b71
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-04-15 19:56:28 -0500

    feat: Add Date Pipe functionality to Basic Page

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import {Component, signal} from '@angular/core';
-import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
+import {Component, effect, signal} from '@angular/core';
+import {LowerCasePipe, TitleCasePipe, UpperCasePipe, DatePipe} from '@angular/common';
 
 @Component({
   selector: 'basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
@@ -12,5 +12,15 @@ export default class BasicPageComponent {
   nameUpper = signal('JHONATAN');
   nameFullName = signal('jHONAtan guErrERo');
 
+  customDate = signal(new Date());
+
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => clearInterval(interval));
+  });
 
 }
