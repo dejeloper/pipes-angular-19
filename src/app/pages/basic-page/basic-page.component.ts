d234c8a53ffa3bed818ad1ffbbacdc13d982cb14
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2025-04-17 21:05:38 -0500

    feat: Update Basic Page layout and locale functionality

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,26 +1,40 @@
-import {Component, effect, signal} from '@angular/core';
-import {LowerCasePipe, TitleCasePipe, UpperCasePipe, DatePipe} from '@angular/common';
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import {Component, effect, inject, LOCALE_ID, signal} from '@angular/core';
+import {AvailableLocale, LocaleService} from '../../services/locale.service';
 
 @Component({
-  selector: 'basic-page',
+  selector: 'app-basic-page',
   imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
+  localeService = inject(LocaleService);
+  currentLocale = signal(inject(LOCALE_ID));
 
-  nameLower = signal('jhonatan');
-  nameUpper = signal('JHONATAN');
-  nameFullName = signal('jHONAtan guErrERo');
+  nameLower = signal('fernando');
+  nameUpper = signal('FERNANDO');
+  fullName = signal('feRnaNdo HeRRerA');
 
   customDate = signal(new Date());
-
 
   tickingDateEffect = effect((onCleanup) => {
     const interval = setInterval(() => {
       this.customDate.set(new Date());
+      console.log('tick');
     }, 1000);
 
-    onCleanup(() => clearInterval(interval));
+    onCleanup(() => {
+      clearInterval(interval);
+    });
   });
 
+  changeLocale(locale: AvailableLocale) {
+    console.log({locale});
+    this.localeService.changeLocale(locale);
+  }
 }
